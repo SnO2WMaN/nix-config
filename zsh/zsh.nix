968bb8fe9eb11413e10ff846f1225bb296a3548d
SnO2WMaN <me@sno2wman.net>
2022-04-14 19:27:48 +0900

    zsh bindkey

--- zsh/zsh.nix
+++ zsh/zsh.nix
@@ -30,5 +30,29 @@
       save = 100000;
       size = 100000;
     };
+
+    initExtra = ''
+      bindkey '^[[A' history-substring-search-up # Up
+      bindkey '^[[B' history-substring-search-down # Down
+      bindkey '^[[D' backward-char # Left
+      bindkey '^[[C' forward-char # Right
+
+      bindkey '^[[1;2D' backward-word
+      bindkey '^[[1;2C' forward-word
+
+      bindkey '^[Od' backward-word # Ctrl+Left
+      bindkey '^[Oc' forward-word # Ctrl+Right
+      bindkey '^H' backward-kill-word # Ctrl+Backspace
+
+      bindkey '^[[2~' overwrite-mode # Insert
+      bindkey '^[[3~' delete-char # Delete
+      bindkey '^[[5~' history-beginning-search-backward # PgUp
+      bindkey '^[[6~' history-beginning-search-forward # PgDn
+      bindkey '^[[7~' beginning-of-line # Home
+      bindkey '^[[8~' end-of-line # End
+      bindkey '^[[H' beginning-of-line
+      bindkey '^[[F' end-of-line
+      bindkey '^[[Z' undo # Shift+Tab 
+    '';
   };
 }
