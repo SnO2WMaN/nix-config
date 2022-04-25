5524a841469ff888c81d55fb71993850048d1802
SnO2WMaN <me@sno2wman.net>
2022-04-26 03:37:44 +0900

    git gnp wip

--- git/git.nix
+++ git/git.nix
@@ -15,6 +15,11 @@
       };
     };
 
+    # signing = {
+    #   key = "346DFF31CF021C4E390BEC749762915DF90A830A";
+    #   signByDefault = true;
+    # };
+
     aliases = {
       c = "commit --verbose";
 
