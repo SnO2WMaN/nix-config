695b09658ef2d6256fcce27dab6757653ab748f6
SnO2WMaN <me@sno2wman.net>
2022-04-27 02:13:33 +0900

    VSCode latest!!

--- vscode/default.nix
+++ vscode/default.nix
@@ -1,6 +1,21 @@
-{ config, pkgs, ... }:
+{ config, ... }:
+let
+  pkgs = import
+    (fetchTarball https://channels.nixos.org/nixos-21.11/nixexprs.tar.xz)
+    { config.allowUnfree = true; };
+in
 {
   nixpkgs.config.allowUnfree = true;
+  services.gnome-keyring.enable = true;
+
+  home.packages = with pkgs; [
+    xdg-utils
+  ];
+
+  home.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+  };
+
   programs.vscode = {
     enable = true;
     package = pkgs.vscode-fhs;
