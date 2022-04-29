bef5958c456e218b751c3f6f3638cea5f49ff80e
SnO2WMaN <me@sno2wman.net>
2022-04-30 03:02:36 +0900

    fx nixos/yukari

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,7 +1,16 @@
-{ nixpkgs, ... }@inputs: {
-  yukari = nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-    specialArgs = inputs;
-    modules = [ ./yukari/configuration.nix ];
+{ nixpkgs, ... }@inputs:
+let
+  nixosConfig =
+    { extraModules ? [ ]
+    }:
+    (nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = inputs;
+      modules = [ ] ++ extraModules;
+    });
+in
+{
+  yukari = nixosConfig {
+    extraModules = [ ./yukari.nix ];
   };
 }
