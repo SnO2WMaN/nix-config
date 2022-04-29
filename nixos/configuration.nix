5fa52841c96165504c537ef9897aee574ae7598e
SnO2WMaN <me@sno2wman.net>
2022-04-30 02:57:43 +0900

    mv to nixos

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,7 +1,7 @@
 { nixpkgs, ... }@inputs: {
   yukari = nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
-    modules = [ ../hosts/yukari/configuration.nix ];
     specialArgs = inputs;
+    modules = [ ./yukari/configuration.nix ];
   };
 }
