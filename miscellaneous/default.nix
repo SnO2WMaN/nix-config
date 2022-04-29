f298580a86161cba852c8cdeef5ece01f6bfb1f6
SnO2WMaN <me@sno2wman.net>
2022-04-29 14:25:20 +0900

    upd miscellaneous

--- miscellaneous/default.nix
+++ miscellaneous/default.nix
@@ -1,42 +1,99 @@
 { config, pkgs, ... }:
 {
   home.packages = with pkgs; [
-    bat
     lsd
     procs
     pueue
     du-dust
     fd
     ripgrep
+    hexyl
+    hyperfine
+    fd
 
     neofetch
     psmisc
-    bottom
 
     curl
     httpie
     wget
+
+    tree
+    file
+    binutils
 
     jq
     yq
     fzf
     skim
 
-    direnv
-
     cachix
     niv
     rnix-lsp
     nix-prefetch-github
     nixpkgs-fmt
+
+    # nettools
+    traceroute
+    tcpdump
+    lsof
+    nmap
+    findomain
+    websocat
+    wireguard-tools
+    ethtool
+    smartmontools # hdd monitor 
+
+    # dev tools
+    act # GitHub Actions Runner 
+    dive # docker image tool 
+    buf # Protobuf tool
+    grpcurl # curl for gRPC 
+    altair # GraphQL GUI 
+    hadolint # linter for Dockerfile
+    treefmt # unified formatter
+    shellcheck
+    git-crypt
+    rustup
+
+    # shell play
+    pipes-rs
+    cbonsai
+    cava # audio visualizer for console
+    tty-clock
+
+    android-studio
+
+    imagemagick
+
+    qt5ct
+
+    gnupg
   ];
 
-  programs.bottom.settings = {
-    flags = {
-      color = "nord";
+  programs.bat = {
+    enable = true;
+    config = {
+      theme = "Nord";
     };
   };
-  programs.bat.config = {
-    theme = "Nord";
+
+  programs.bottom = {
+    enable = true;
+    settings = {
+      flags = {
+        color = "nord";
+      };
+    };
   };
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv = {
+      enable = true;
+    };
+  };
+
+  programs.command-not-found.enable = true;
+  programs.dircolors.enable = true;
 }
