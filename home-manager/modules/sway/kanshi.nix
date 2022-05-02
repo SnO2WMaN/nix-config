f7eb5f288b9627ab3ac0cf71023da6079113c777
SnO2WMaN <me@sno2wman.net>
2022-05-02 16:44:35 +0900

    rm kanshi

--- home-manager/modules/sway/kanshi.nix
+++ home-manager/modules/sway/kanshi.nix
@@ -1,6 +1,28 @@
 { config, pkgs, lib, ... }:
 {
-  home.packages = with pkgs; [ kanshi ];
-
-  xdg.configFile."kanshi/config".source = ./kanshi/config;
+  services.kanshi = {
+    enable = true;
+    profiles = {
+      yukari = {
+        outputs = [
+          {
+            criteria = "DP-1";
+            position = "0,0";
+            mode = "1920x1080";
+            transform = "270";
+          }
+          {
+            criteria = "DP-2";
+            position = "1200,0";
+            mode = "2560x1080";
+          }
+          {
+            criteria = "DP-3";
+            position = "1200,0";
+            mode = "2560x1080";
+          }
+        ];
+      };
+    };
+  };
 }
