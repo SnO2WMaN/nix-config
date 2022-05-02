bc4510f75ba944d60208c0a82e9d1d5311426f56
SnO2WMaN <me@sno2wman.net>
2022-05-02 16:43:36 +0900

    sway軽微な修正

--- home-manager/modules/sway/sway.nix
+++ home-manager/modules/sway/sway.nix
@@ -53,12 +53,11 @@ in
         {
           command =
             ''
-              swayidle -w \
+              ${pkgs.swayidle}/bin/swayidle -w \
                 timeout 1200 'swaymsg "output * dpms off"' \
                 resume 'swaymsg "output * dpms on"'
             '';
         }
-
       ];
       bars = [
         { command = "${pkgs.waybar}/bin/waybar"; }
@@ -70,22 +69,40 @@ in
         names = [ "JetBrains Mono" ];
         size = 8.0;
       };
+      floating = {
+        criteria = [
+          { app_id = "pavucontrol"; }
+          { app_id = "nm-connection-editor"; }
+          { app_id = "pcmanfm"; }
+          { app_id = "ristretto"; }
+          { app_id = "zoom"; }
+          { app_id = "org.larusstone.qtspim"; }
+          { app_id = "org.fcitx.fcitx5-config-qt"; }
+          { instance = "fontforge"; }
+          { instance = "1password"; }
+          { instance = "qpaeq"; }
+        ];
+      };
       keybindings =
         let
           modifier = config.wayland.windowManager.sway.config.modifier;
         in
-        lib.mkOptionDefault {
-          "${modifier}+0" = "workspace number 10";
-          "${modifier}+Shift+0" = "move container to workspace number 10";
-          "${modifier}+Shift+Delete" = "kill";
+        lib.mkOptionDefault
+          {
+            "${modifier}+0" = "workspace number 10";
+            "${modifier}+Shift+0" = "move container to workspace number 10";
+            "${modifier}+Shift+Delete" = "kill";
             "${modifier}+g" = "exec --no-startup-id wofi_ghq";
-          "${modifier}+Shift+S" = "exec ${pkgs.sway-contrib.grimshot}/bin/grimshot --notify save area $HOME/Pictures/screenshots/$(date +\"%Y_%m_%d__%H_%M_%S\").png";
-          "XF86AudioRaiseVolume" = "exec pactl set-sink-volume 0 +5%";
-          "XF86AudioLowerVolume" = "exec pactl set-sink-volume 0 -5%";
-          "XF86AudioMute" = "exec pactl set-sink-mute 0 toggle";
-          "XF86MonBrightnessUp" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set 5%+";
-          "XF86MonBrightnessDown" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set 5%-";
-        };
+            "${modifier}+F1" = "exec ${pkgs.vscode}/bin/code";
+            "${modifier}+F2" = "exec ${pkgs.firefox}/bin/firefox";
+            "${modifier}+P" = "exec ${pkgs.pcmanfm}/bin/pcmanfm";
+            "${modifier}+Shift+S" = "exec ${pkgs.sway-contrib.grimshot}/bin/grimshot --notify save area $HOME/Pictures/screenshots/$(date +\"%Y_%m_%d__%H_%M_%S\").png";
+            "XF86AudioRaiseVolume" = "exec pactl set-sink-volume 0 +5%";
+            "XF86AudioLowerVolume" = "exec pactl set-sink-volume 0 -5%";
+            "XF86AudioMute" = "exec pactl set-sink-mute 0 toggle";
+            "XF86MonBrightnessUp" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set 5%+";
+            "XF86MonBrightnessDown" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set 5%-";
+          };
     };
   };
 
