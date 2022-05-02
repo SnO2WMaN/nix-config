f46a65aa4793160d9c2c0f46e73f888569b74083
SnO2WMaN <me@sno2wman.net>
2022-05-02 16:44:54 +0900

    fx md-to-pdf?

--- home-manager/overlays/node-packages.nix
+++ home-manager/overlays/node-packages.nix
@@ -1,3 +1,19 @@
-final: prev: {
-  nodePackages = prev.nodePackages // (import ./node2nix { pkgs = final; });
+final: prev:
+let
+  nodePackage = (import ./node2nix { pkgs = final; });
+in
+{
+  nodePackages =
+    prev.nodePackages //
+    nodePackage //
+    {
+      md-to-pdf = nodePackage.md-to-pdf.override {
+        buildInputs = [ final.chromium ];
+
+        preInstallPhases = "skipPuppeteerDownload";
+        skipPuppeteerDownload = ''
+          export PUPPETEER_SKIP_DOWNLOAD=1
+        '';
+      };
+    };
 }
