5b3b38756356dfddbc35517baa2dc2da242e9bb5
SnO2WMaN <me@sno2wman.net>
2022-05-10 14:06:33 +0900

    vscode-extensions flake

--- flakes/vscode-extensions/flake.nix
+++ flakes/vscode-extensions/flake.nix
@@ -1,26 +1,25 @@
 {
   description = "Nix flake for VSCode extensions";
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+  };
   outputs = { ... }@inputs: {
-
-    /*
-      overlay = final: prev: {
-      vscode-extensions = with final.vscode-utils; prev.vscode-extensions // {
-      ahmadalli.vscode-nginx-conf = extensionFromVscodeMarketplace {
-      name = "vscode-nginx-conf";
-      publisher = "ahmadalli";
-      version = "0.1.3";
-      sha256 = "10z0him4kl9q6h1nip7d3dp9nv0a1dkh3x6zqc6nilfw959v3358";
-      };
-      };
-      };
-    */
     overlay = final: prev: {
-      vscode-extensions = with final.vscode-utils;
-        prev.vscode-extensions //
-        builtins.listToAttrs (map
-          (ext: { name = "${ext.publisher}.${ext.name}"; value = extensionFromVscodeMarketplace ext; })
+      vscode-extensions = prev.vscode-extensions //
+        /*
+          builtins.listToAttrs (map
+          (ext: {
+          name = "${ext.publisher}.${ext.name}"; # it can't 
+          value = final.vscode-utils.extensionFromVscodeMarketplace ext;
+          })
           (import ./extensions.nix).extensions
-        );
+          );
+        */
+        (import ./defaults.nix) ({
+          inherit (final.vscode-utils) extensionFromVscodeMarketplace;
+        });
     };
   };
 }
