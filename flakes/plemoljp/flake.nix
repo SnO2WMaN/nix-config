c1f3129a99c71b2573293e7afc5c35df30054d61
SnO2WMaN <me@sno2wman.net>
2022-05-10 15:59:18 +0900

    add plemoljp flake wip

---
+++ flakes/plemoljp/flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Nix flake for PlemolJP";
+
+  inputs = {
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system: rec {
+    });
+}
