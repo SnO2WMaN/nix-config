ad139459655f98d3d3f95799e8c554aad69856b7
SnO2WMaN <me@sno2wman.net>
2022-05-13 22:05:34 +0900

    add chrome

---
+++ home-manager/modules/chrome/default.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    google-chrome
+  ];
+  home.sessionVariables = {
+    CHROME_EXECUTABLE = "${pkgs.google-chrome}/bin/google-chrome-stable";
+  };
+}
