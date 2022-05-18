85e4c8c3d327d474388606b1953c6b15a04221d0
SnO2WMaN <me@sno2wman.net>
2022-05-18 11:42:23 +0900

    upd QT_QPA_PLATFORM in sway for android emu

--- home-manager/modules/sway/sway.nix
+++ home-manager/modules/sway/sway.nix
@@ -37,7 +37,7 @@ in
       ''
         export XDG_SESSION_TYPE=wayland
         export XDG_CURRENT_DESKTOP=sway
-        export QT_QPA_PLATFORM=wayland
+        export QT_QPA_PLATFORM="wayland;xcb"
         export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
         export _JAVA_AWT_WM_NONREPARENTING=1
       '';
