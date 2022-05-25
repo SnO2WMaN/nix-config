ae24463ce1e2e2506ade4f1d097aa3e33e7b2892
SnO2WMaN <me@sno2wman.net>
2022-05-26 00:40:44 +0900

    android周り

--- home-manager/modules/android/default.nix
+++ home-manager/modules/android/default.nix
@@ -1,6 +1,18 @@
 { config, pkgs, ... }:
 {
   home.packages = with pkgs; [
+    android-tools
     android-studio
+    nubank.dart
+    nubank.flutter
+    nubank.flutter-patch
   ];
+  home.sessionVariables = {
+    ANDROID_SDK_ROOT = "${builtins.getEnv "HOME"}/Android/Sdk";
+  };
+  home.sessionPath = [
+    "${builtins.getEnv "HOME"}/Android/Sdk/platform-tools"
+    "${builtins.getEnv "HOME"}/.pub-cache/bin"
+  ];
+  # programs.git.extraConfig.safe.directory = [ "${pkgs.flutter}" ];
 }
