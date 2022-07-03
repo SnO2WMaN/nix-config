10b5a88ddfc145ac618fcfa8fbff948e38590d62
SnO2WMaN <me@sno2wman.net>
2022-07-04 02:58:16 +0900

    add nixpkgs-wayland cachix

--- nixos/modules/nix/default.nix
+++ nixos/modules/nix/default.nix
@@ -27,10 +27,12 @@
       "https://cache.nixos.org"
       "https://sno2wman.cachix.org"
       "https://dhall.cachix.org"
+      "https://nixpkgs-wayland.cachix.org"
     ];
     binaryCachePublicKeys = [
       "sno2wman.cachix.org-1:JHDNKuz+q1xthbonwirDQzMZtwPrDnwCq3wUX3kmBVU="
       "dhall.cachix.org-1:8laGciue2JBwD49ICFtg+cIF8ddDaW7OFBjDb/dHEAo="
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
     ];
   };
 }
