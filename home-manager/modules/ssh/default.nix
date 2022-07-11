5c31d2956322b687f78fc4d6124ead876dbd79ac
SnO2WMaN <me@sno2wman.net>
2022-07-11 11:05:08 +0900

    add ssh patch

--- home-manager/modules/ssh/default.nix
+++ home-manager/modules/ssh/default.nix
@@ -3,6 +3,15 @@
   pkgs,
   ...
 }: {
+  nixpkgs.overlays = [
+    (final: prev: {
+      openssh = prev.openssh.overrideAttrs (old: {
+        patches = (old.patches or []) ++ [./openssh.patch];
+        doCheck = false;
+      });
+    })
+  ];
+
   programs.ssh = {
     enable = true;
     matchBlocks = with builtins; let
