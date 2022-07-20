77f46cdd1113d908dbd587a3770bd8faa1411e35
SnO2WMaN <me@sno2wman.net>
2022-07-20 22:20:19 +0900

    hm(yukari): pin gitkraken 8.7.0

--- home-manager/modules/desktop/gitkraken.nix
+++ home-manager/modules/desktop/gitkraken.nix
@@ -6,7 +6,18 @@
   nixpkgs.config.allowUnfree = true;
 
   home.packages = with pkgs; [
-    gitkraken
+    (gitkraken.overrideAttrs (old: let
+      version = "8.7.0";
+      srcs = {
+        x86_64-linux = fetchzip {
+          url = "https://release.axocdn.com/linux/GitKraken-v${version}.tar.gz";
+          sha256 = "sha256-+Q6sw+V/+tdBaKmR3l0ARq7Z3h/yu06af4PtG2JESgQ=";
+        };
+      };
+    in {
+      inherit version;
+      src = srcs.${pkgs.stdenv.hostPlatform.system};
+    }))
   ];
 
   xdg.mimeApps = {
