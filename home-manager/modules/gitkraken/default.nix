74815fd54ce5516c97fa6c0212742a8919a3fde0
SnO2WMaN <me@sno2wman.net>
2022-08-17 00:19:50 +0900

    unpin gitkraken version

--- home-manager/modules/gitkraken/default.nix
+++ home-manager/modules/gitkraken/default.nix
@@ -4,18 +4,7 @@
   ...
 }: {
   home.packages = with pkgs; [
-    (gitkraken.overrideAttrs (old: let
-      version = "8.7.0";
-      srcs = {
-        x86_64-linux = fetchzip {
-          url = "https://release.axocdn.com/linux/GitKraken-v${version}.tar.gz";
-          sha256 = "sha256-+Q6sw+V/+tdBaKmR3l0ARq7Z3h/yu06af4PtG2JESgQ=";
-        };
-      };
-    in {
-      inherit version;
-      src = srcs.${pkgs.stdenv.hostPlatform.system};
-    }))
+    gitkraken
   ];
 
   xdg.mimeApps = {
