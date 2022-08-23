3b9bfd74c9ae99ec7a193f9ecaaa155413246fe5
SnO2WMaN <me@sno2wman.net>
2022-08-23 14:28:39 +0900

    Update flake and ignore nixgl

--- modules/base/overlays.nix
+++ modules/base/overlays.nix
@@ -8,7 +8,7 @@
   nixpkgs.overlays = [
     self.overlays.default
     nixpkgs-wayland.overlay
-    nixgl.overlay
+    # nixgl.overlay TODO: fix when https://github.com/guibou/nixGL/issues/111
     useful-scripts.overlays.default
   ];
 }
