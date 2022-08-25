afd84bddef8351043a6264b8e6385c75af92d396
SnO2WMaN <me@sno2wman.net>
2022-08-25 13:12:22 +0900

    やっぱりnordに

--- modules/base/stylix.nix
+++ modules/base/stylix.nix
@@ -13,8 +13,8 @@
     url = "https://freight.cargo.site/t/original/i/b32f61d8058742b67ac706434c3b1aec306e9bb8e640bdc05a70d7f5e509c65d/ED_33.jpg";
     sha256 = "sha256-TrnzJBx7WpKKpskBpBYfnnIfIhlhEQG+7KkSCsMDtsE=";
   };
-  stylix.polarity = "dark";
-  # stylix.base16Scheme = "${base16-schemes}/nord.yaml";
+  #  stylix.polarity = "dark";
+  stylix.base16Scheme = "${base16-schemes}/nord.yaml";
   stylix.fonts = {
     serif = {
       package = pkgs.ipaexfont;
