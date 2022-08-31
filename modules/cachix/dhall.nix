e3bb3f3a74f69e22e7f8f84469ab2b42048207ee
SnO2WMaN <me@sno2wman.net>
2022-08-31 22:32:42 +0900

    dhall cache

--- modules/cachix/dhall.nix
+++ modules/cachix/dhall.nix
@@ -1,9 +1,11 @@
 {
   nix.settings = {
     substituters = [
+      "https://cache.dhall-lang.org"
       "https://dhall.cachix.org"
     ];
     trusted-public-keys = [
+      "cache.dhall-lang.org:I9/H18WHd60olG5GsIjolp7CtepSgJmM2CsO813VTmM="
       "dhall.cachix.org-1:8laGciue2JBwD49ICFtg+cIF8ddDaW7OFBjDb/dHEAo="
     ];
   };
