88d61cec3af868a718ce7a812523dc048eefc411
SnO2WMaN <me@sno2wman.net>
2022-09-20 16:43:47 +0900

    discord

--- home-manager/modules/discord/default.nix
+++ home-manager/modules/discord/default.nix
@@ -4,6 +4,13 @@
   ...
 }: {
   home.packages = with pkgs; [
-    discord
+    (discord.overrideAttrs
+      (oa: rec {
+        version = "0.0.20";
+        srcs = fetchurl {
+          url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
+          sha256 = "sha256-3f7yuxigEF3e8qhCetCHKBtV4XUHsx/iYiaCCXjspYw=";
+        };
+      }))
   ];
 }
