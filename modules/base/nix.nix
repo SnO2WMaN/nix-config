75a1194f8c058f9a07662843ba9ea512503df85b
SnO2WMaN <me@sno2wman.net>
2022-09-22 17:50:48 +0900

    nix remote build

--- modules/base/nix.nix
+++ modules/base/nix.nix
@@ -1,28 +1,50 @@
 {
+  config,
   pkgs,
   lib,
   inputs,
-  config,
   ...
 }: {
   nix = {
     package = pkgs.nixFlakes;
+    distributedBuilds = true;
+
     extraOptions = ''
       experimental-features = nix-command flakes
       builders-use-substitutes = true
       keep-outputs = true
       keep-derivations = true
     '';
-  };
-  nix.gc = {
-    automatic = true;
-    dates = "monthly";
-    options = "--delete-older-than 30d";
-  };
-  nix.settings = {
-    max-jobs = lib.mkDefault 8;
-    auto-optimise-store = true;
-    trusted-users = ["root" "sno2wman"];
-    substituters = ["https://cache.nixos.org/"];
+
+    gc = {
+      automatic = true;
+      dates = "monthly";
+      options = "--delete-older-than 30d";
+    };
+
+    settings = {
+      max-jobs = lib.mkDefault 8;
+      auto-optimise-store = true;
+      trusted-users = ["root" "sno2wman"];
+      substituters = ["https://cache.nixos.org/"];
+    };
+
+    buildMachines = lib.filter (x: x.hostName != config.networking.hostName) [
+      {
+        hostName = "remilia";
+        system = "x86_64-linux";
+        sshUser = "root";
+        sshKey = "/home/sno2wman/.ssh/id_25519";
+        maxJobs = 24;
+        speedFactor = 2;
+        supportedFeatures = [
+          "nixos-test"
+          "benchmark"
+          "big-parallel"
+          "kvm"
+        ];
+        mandatoryFeatures = [];
+      }
+    ];
   };
 }
