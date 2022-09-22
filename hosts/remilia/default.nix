75f0eb96e116051dcd258b708999b50e26e76e02
SnO2WMaN <me@sno2wman.net>
2022-09-22 19:33:10 +0900

    add sno2wman ssh key

--- hosts/remilia/default.nix
+++ hosts/remilia/default.nix
@@ -110,6 +110,9 @@
       "libvirtd"
       "kvm"
     ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ02RYFEONAr/5a3fokBYHUFVPqF8G64DxhV5RGu7gtK me@sno2wman.net"
+    ];
   };
 
   services.vscode-server.enable = true;
